e17370fe226902abf02875de03cf88f0f56fc90b
R3mon3 <orhan.turkoglu@uoit.net>
2015-09-23 15:18:21 -0400

    Create Lab1-Step-36.php

---
+++ Lab1-Step-36.php
@@ -0,0 +1,13 @@
+<?php
+$a1=$_POST ["a"];
+$b1=$_POST ["b"];
+$c1=$_POST ["c"];
+$d1=$_POST ["d"];
+$e1=$_POST ["e"];
+
+echo "<p>Greetings $a1 $b1</p>";
+echo "<p>Your User Name is: $c1 and Your Password: $d1</p>";
+echo "<p>Your Cellphone Number is: $e1</p>";
+
+echo "<p><hr color='darkblue' size='4'>End of Part3</p>";
+?>
